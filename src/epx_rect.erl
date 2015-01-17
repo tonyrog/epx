da28d3f0715b9e6ba92388990ba1f1c9a69abac7
Tony Rogvall <tony@rogvall.se>
2015-01-17 02:11:49 +0100

    removed commented out source code, fixed some rectangle stuff

--- src/epx_rect.erl
+++ src/epx_rect.erl
@@ -29,10 +29,10 @@ union(undefined, R) -> R;
 union(R, undefined) -> R;
 union({X1,Y1,W1,H1}, {X2,Y2,W2,H2}) ->
     Xl = erlang:min(X1,X2),
-    Xr = erlang:max(X1+W1-1, X2+W2-1),
+    Xr = erlang:max(X1+W1, X2+W2),
     Yt = erlang:min(Y1,Y2),
-    Yb = erlang:max(Y1+H1-1, Y2+H2-1),
-    {Xl, Yt, Xr-Xl+1, Yb-Yt+1}.
+    Yb = erlang:max(Y1+H1, Y2+H2),
+    {Xl, Yt, Xr-Xl, Yb-Yt}.
 
 union([R]) -> R;
 union([R|Rs]) ->
@@ -42,16 +42,16 @@ intersect(undefined, R) -> R;
 intersect(R, undefined) -> R;
 intersect({X1,Y1,W1,H1}, {X2,Y2,W2,H2}) ->
     Xl = erlang:max(X1,X2),
-    Xr = erlang:min(X1+W1-1, X2+W2-1),
-    if Xl > Xr ->
+    Xr = erlang:min(X1+W1, X2+W2),
+    if Xl >= Xr ->
 	    {0,0,0,0};
        true ->
 	    Yt = erlang:max(Y1,Y2),
-	    Yb = erlang:min(Y1+H1-1, Y2+H2-1),
-	    if Yt > Yb ->
+	    Yb = erlang:min(Y1+H1, Y2+H2),
+	    if Yt >= Yb ->
 		    {0,0,0,0};
 	       true ->
-		    {Xl, Yt, Xr-Xl+1, Yb-Yt+1}
+		    {Xl, Yt, Xr-Xl, Yb-Yt}
 	    end
     end.
 
@@ -62,23 +62,9 @@ intersect([R|Rs]) ->
 %% is Rect A a proper sub-rect of B (intersect(A,B) == A)
 is_subrect(_A, undefined) -> true;
 is_subrect({Xa,Ya,Wa,Ha}, {Xb,Yb,Wb,Hb}) ->
-    (Xa >= Xb) andalso ((Xa+Wa-1) =< (Xb+Wb-1)) andalso
-    (Ya >= Yb) andalso ((Ya+Ha-1) =< (Yb+Hb-1)).
+    (Xa >= Xb) andalso ((Xa+Wa) =< (Xb+Wb)) andalso
+    (Ya >= Yb) andalso ((Ya+Ha) =< (Yb+Hb)).
 
 %% Add Point P to rectangle A = union(A, {Px,Py,1,1})
-add_point(undefined, {Xp,Yp}) ->
-    {Xp,Yp,1,1};
-add_point({Xa,Ya,Wa,Ha}, {Xp,Yp}) ->
-    Xl = erlang:min(Xa, Xp),
-    Xr = erlang:max(Xa+Wa-1, Xp+1),
-    Yt = erlang:min(Ya, Yp),
-    Yb = erlang:max(Ya+Ha-1, Yp+1),
-    {Xl, Yt, Xr-Xl+1, Yb-Yt+1}.    
-    
-
-
-       
-
-
-    
-
+add_point(undefined, {Xp,Yp}) -> {Xp,Yp,1,1};
+add_point(A, {Xp,Yp}) -> union(A, {Xp,Yp,1,1}).
