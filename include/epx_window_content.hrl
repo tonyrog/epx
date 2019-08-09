8c911bfeff9c21575e158a2769826cccdff59e10
Tony Rogvall <tony@rogvall.se>
2019-08-09 13:20:14 +0200

    started adding view handling

---
+++ include/epx_window_content.hrl
@@ -0,0 +1,79 @@
+%%% @author Tony Rogvall <tony@rogvall.se>
+%%% @copyright (C) 2019, Tony Rogvall
+%%% @doc
+%%%    epx window content utils
+%%% @end
+%%% Created :  2 Jul 2019 by Tony Rogvall <tony@rogvall.se>
+
+-ifndef(__EPX_WINDOW_CONTENT_HRL__).
+-define(__EPX_WINDOW_CONTENT_HRL__, true).
+
+%%
+%% window_info cover items that are mainly used to determine
+%% size information before drawing etc.
+%%
+-record(window_info,
+	{
+	 font_name,
+	 font_size,
+	 font,
+	 gc,
+	 glyph_width,
+	 glyph_height,
+	 glyph_ascent,
+	 glyph_descent,
+	 top_bar           = 0,
+	 left_bar          = 0,
+	 right_bar         = 0,
+	 bottom_bar        = 0,
+	 top_offset        = 5,
+	 left_offset       = 5,
+	 right_offset      = 5,
+	 bottom_offset     = 5,
+	 scroll_bar_size   = 16,
+	 scroll_hndl_size  = 10,
+	 scroll_xstep      = 4,
+	 scroll_ystep      = 4,
+	 scroll_horizontal = bottom,
+	 scroll_vertical   = right
+	}).
+
+%%
+%% window_profile contains color information
+%%
+-record(window_profile,
+	{
+	 scheme :: atom(),
+	 font_name               = "Arial",
+	 font_size               = 14,
+	 font_color              = grey5,
+	 background_color        = grey10,
+	 scroll_bar_color        = grey12,   %% 94%
+	 scroll_hndl_color       = grey15,   %% 50%
+	 top_bar_color           = red,
+	 left_bar_color          = green,
+	 right_bar_color         = blue,
+	 bottom_bar_color        = grey5,
+	 scroll_horizontal       = right,
+	 scroll_vertical         = bottom
+	}).
+
+-record(window_content,
+	{
+	 profile :: #window_profile {},
+	 view_xpos  = 0,  %% scroll x
+	 view_ypos  = 0,  %% scroll y
+	 view_left  = 0   :: integer(),
+	 view_right = 0   :: integer(),
+	 view_top   = 0   :: integer(),
+	 view_bottom = 0  :: integer(),
+	 hscroll          :: undefined | epx:epx_rect(),
+	 hhndl            :: undefined | epx:epx_rect(),
+	 vscroll          :: undefined | epx:epx_rect(),
+	 vhndl            :: undefined | epx:epx_rect(),
+	 motion           :: undefined |
+			     {vhndl,epx:epx_point()} |
+			     {hhndl,epx:epx_point()}	 
+	 }).
+
+-endif.
---
+++ src/crayola.hrl
@@ -0,0 +1,318 @@
+?COLORNAME("Red", 	{16#ED,16#0A,16#3F}).
+?COLORNAME("Maroon", 	{16#C3,16#21,16#48}).
+?COLORNAME("Scarlet", 	{16#FD,16#0E,16#35}).
+?COLORNAME("Brick Red", 	{16#C6,16#2D,16#42}).
+?COLORNAME("English Vermilion", 	{16#CC,16#47,16#4B}).
+?COLORNAME("Madder Lake", 	{16#CC,16#33,16#36}).
+?COLORNAME("Permanent Geranium Lake", 	{16#E1,16#2C,16#2C}).
+?COLORNAME("Maximum Red", 	{16#D9,16#21,16#21}).
+?COLORNAME("Indian Red", 	{16#B9,16#4E,16#48}).
+?COLORNAME("Orange-Red", 	{16#FF,16#53,16#49}).
+?COLORNAME("Sunset Orange", 	{16#FE,16#4C,16#40}).
+?COLORNAME("Bittersweet", 	{16#FE,16#6F,16#5E}).
+?COLORNAME("Dark Venetian Red", 	{16#B3,16#3B,16#24}).
+?COLORNAME("Venetian Red", 	{16#CC,16#55,16#3D}).
+?COLORNAME("Light Venetian Red", 	{16#E6,16#73,16#5C}).
+?COLORNAME("Vivid Tangerine", 	{16#FF,16#99,16#80}).
+?COLORNAME("Middle Red", 	{16#E5,16#8E,16#73}).
+?COLORNAME("Burnt Orange", 	{16#FF,16#7F,16#49}).
+?COLORNAME("Red-Orange", 	{16#FF,16#68,16#1F}).
+?COLORNAME("Orange", 	{16#FF,16#88,16#33}).
+?COLORNAME("Macaroni and Cheese", 	{16#FF,16#B9,16#7B}).
+?COLORNAME("Middle Yellow Red", 	{16#EC,16#B1,16#76}).
+?COLORNAME("Mango Tango", 	{16#E7,16#72,16#00}).
+?COLORNAME("Yellow-Orange", 	{16#FF,16#AE,16#42}).
+?COLORNAME("Maximum Yellow Red", 	{16#F2,16#BA,16#49}).
+?COLORNAME("Banana Mania", 	{16#FB,16#E7,16#B2}).
+?COLORNAME("Maize", 	{16#F2,16#C6,16#49}).
+?COLORNAME("Orange-Yellow", 	{16#F8,16#D5,16#68}).
+?COLORNAME("Goldenrod", 	{16#FC,16#D6,16#67}).
+?COLORNAME("Dandelion", 	{16#FE,16#D8,16#5D}).
+?COLORNAME("Yellow", 	{16#FB,16#E8,16#70}).
+?COLORNAME("Green-Yellow", 	{16#F1,16#E7,16#88}).
+?COLORNAME("Middle Yellow", 	{16#FF,16#EB,16#00}).
+?COLORNAME("Olive Green", 	{16#B5,16#B3,16#5C}).
+?COLORNAME("Spring Green", 	{16#EC,16#EB,16#BD}).
+?COLORNAME("Maximum Yellow", 	{16#FA,16#FA,16#37}).
+?COLORNAME("Canary", 	{16#FF,16#FF,16#99}).
+?COLORNAME("Lemon Yellow", 	{16#FF,16#FF,16#9F}).
+?COLORNAME("Maximum Green Yellow", 	{16#D9,16#E6,16#50}).
+?COLORNAME("Middle Green Yellow", 	{16#AC,16#BF,16#60}).
+?COLORNAME("Inchworm", 	{16#AF,16#E3,16#13}).
+?COLORNAME("Light Chrome Green", 	{16#BE,16#E6,16#4B}).
+?COLORNAME("Yellow-Green", 	{16#C5,16#E1,16#7A}).
+?COLORNAME("Maximum Green", 	{16#5E,16#8C,16#31}).
+?COLORNAME("Asparagus", 	{16#7B,16#A0,16#5B}).
+?COLORNAME("Granny Smith Apple", 	{16#9D,16#E0,16#93}).
+?COLORNAME("Fern", 	{16#63,16#B7,16#6C}).
+?COLORNAME("Middle Green", 	{16#4D,16#8C,16#57}).
+?COLORNAME("Green", 	{16#3A,16#A6,16#55}).
+?COLORNAME("Medium Chrome Green", 	{16#6C,16#A6,16#7C}).
+?COLORNAME("Forest Green", 	{16#5F,16#A7,16#77}).
+?COLORNAME("Sea Green", 	{16#93,16#DF,16#B8}).
+?COLORNAME("Shamrock", 	{16#33,16#CC,16#99}).
+?COLORNAME("Mountain Meadow", 	{16#1A,16#B3,16#85}).
+?COLORNAME("Jungle Green", 	{16#29,16#AB,16#87}).
+?COLORNAME("Caribbean Green", 	{16#00,16#CC,16#99}).
+?COLORNAME("Tropical Rain Forest", 	{16#00,16#75,16#5E}).
+?COLORNAME("Middle Blue Green", 	{16#8D,16#D9,16#CC}).
+?COLORNAME("Pine Green", 	{16#01,16#78,16#6F}).
+?COLORNAME("Maximum Blue Green", 	{16#30,16#BF,16#BF}).
+?COLORNAME("Robin's Egg Blue", 	{16#00,16#CC,16#CC}).
+?COLORNAME("Teal Blue", 	{16#00,16#80,16#80}).
+?COLORNAME("Light Blue", 	{16#8F,16#D8,16#D8}).
+?COLORNAME("Aquamarine", 	{16#95,16#E0,16#E8}).
+?COLORNAME("Turquoise Blue", 	{16#6C,16#DA,16#E7}).
+?COLORNAME("Outer Space", 	{16#2D,16#38,16#3A}).
+?COLORNAME("Sky Blue", 	{16#76,16#D7,16#EA}).
+?COLORNAME("Middle Blue", 	{16#7E,16#D4,16#E6}).
+?COLORNAME("Blue-Green", 	{16#00,16#95,16#B7}).
+?COLORNAME("Pacific Blue", 	{16#00,16#9D,16#C4}).
+?COLORNAME("Cerulean", 	{16#02,16#A4,16#D3}).
+?COLORNAME("Maximum Blue", 	{16#47,16#AB,16#CC}).
+?COLORNAME("Blue (I)", 	{16#49,16#97,16#D0}).
+?COLORNAME("Cerulean Blue", 	{16#33,16#9A,16#CC}).
+?COLORNAME("Cornflower", 	{16#93,16#CC,16#EA}).
+?COLORNAME("Green-Blue", 	{16#28,16#87,16#C8}).
+?COLORNAME("Midnight Blue", 	{16#00,16#46,16#8C}).
+?COLORNAME("Navy Blue", 	{16#00,16#66,16#CC}).
+?COLORNAME("Denim", 	{16#15,16#60,16#BD}).
+?COLORNAME("Blue (III)", {16#00,16#66,16#FF}).
+?COLORNAME("Cadet Blue", 	{16#A9,16#B2,16#C3}).
+?COLORNAME("Periwinkle", 	{16#C3,16#CD,16#E6}).
+?COLORNAME("Blue (II)", 	{16#45,16#70,16#E6}).
+?COLORNAME("Wild Blue Yonder", 	{16#7A,16#89,16#B8}).
+?COLORNAME("Indigo", 	{16#4F,16#69,16#C6}).
+?COLORNAME("Manatee", 	{16#8D,16#90,16#A1}).
+?COLORNAME("Cobalt Blue", 	{16#8C,16#90,16#C8}).
+?COLORNAME("Celestial Blue", 	{16#70,16#70,16#CC}).
+?COLORNAME("Blue Bell", 	{16#99,16#99,16#CC}).
+?COLORNAME("Maximum Blue Purple", 	{16#AC,16#AC,16#E6}).
+?COLORNAME("Violet-Blue", 	{16#76,16#6E,16#C8}).
+?COLORNAME("Blue-Violet", 	{16#64,16#56,16#B7}).
+?COLORNAME("Ultramarine Blue", 	{16#3F,16#26,16#BF}).
+?COLORNAME("Middle Blue Purple", 	{16#8B,16#72,16#BE}).
+?COLORNAME("Purple Heart", 	{16#65,16#2D,16#C1}).
+?COLORNAME("Royal Purple", 	{16#6B,16#3F,16#A0}).
+?COLORNAME("Violet (II)", 	{16#83,16#59,16#A3}).
+?COLORNAME("Medium Violet", 	{16#8F,16#47,16#B3}).
+?COLORNAME("Wisteria", 	{16#C9,16#A0,16#DC}).
+?COLORNAME("Lavender (I)", 	{16#BF,16#8F,16#CC}).
+?COLORNAME("Vivid Violet", 	{16#80,16#37,16#90}).
+?COLORNAME("Maximum Purple", 	{16#73,16#33,16#80}).
+?COLORNAME("Purple Mountains' Majesty", 	{16#D6,16#AE,16#DD}).
+?COLORNAME("Fuchsia", 	{16#C1,16#54,16#C1}).
+?COLORNAME("Pink Flamingo", 	{16#FC,16#74,16#FD}).
+?COLORNAME("Violet (I)", 	{16#73,16#2E,16#6C}).
+?COLORNAME("Brilliant Rose", 	{16#E6,16#67,16#CE}).
+?COLORNAME("Orchid", 	{16#E2,16#9C,16#D2}).
+?COLORNAME("Plum", 	{16#8E,16#31,16#79}).
+?COLORNAME("Medium Rose", 	{16#D9,16#6C,16#BE}).
+?COLORNAME("Thistle", 	{16#EB,16#B0,16#D7}).
+?COLORNAME("Mulberry", 	{16#C8,16#50,16#9B}).
+?COLORNAME("Red-Violet", 	{16#BB,16#33,16#85}).
+?COLORNAME("Middle Purple", 	{16#D9,16#82,16#B5}).
+?COLORNAME("Maximum Red Purple", 	{16#A6,16#3A,16#79}).
+?COLORNAME("Jazzberry Jam", 	{16#A5,16#0B,16#5E}).
+?COLORNAME("Eggplant", 	{16#61,16#40,16#51}).
+?COLORNAME("Magenta", 	{16#F6,16#53,16#A6}).
+?COLORNAME("Cerise", 	{16#DA,16#32,16#87}).
+?COLORNAME("Wild Strawberry", 	{16#FF,16#33,16#99}).
+?COLORNAME("Lavender (II)", 	{16#FB,16#AE,16#D2}).
+?COLORNAME("Cotton Candy", 	{16#FF,16#B7,16#D5}).
+?COLORNAME("Carnation Pink", 	{16#FF,16#A6,16#C9}).
+?COLORNAME("Violet-Red", 	{16#F7,16#46,16#8A}).
+?COLORNAME("Razzmatazz", 	{16#E3,16#0B,16#5C}).
+?COLORNAME("Pig Pink", 	{16#FD,16#D7,16#E4}).
+?COLORNAME("Carmine", 	{16#E6,16#2E,16#6B}).
+?COLORNAME("Blush", 	{16#DB,16#50,16#79}).
+?COLORNAME("Tickle Me Pink", 	{16#FC,16#80,16#A5}).
+?COLORNAME("Mauvelous", 	{16#F0,16#91,16#A9}).
+?COLORNAME("Salmon", 	{16#FF,16#91,16#A4}).
+?COLORNAME("Middle Red Purple", 	{16#A5,16#53,16#53}).
+?COLORNAME("Mahogany", 	{16#CA,16#34,16#35}).
+?COLORNAME("Melon", 	{16#FE,16#BA,16#AD}).
+?COLORNAME("Pink Sherbert", 	{16#F7,16#A3,16#8E}).
+?COLORNAME("Burnt Sienna", 	{16#E9,16#74,16#51}).
+?COLORNAME("Brown", 	{16#AF,16#59,16#3E}).
+?COLORNAME("Sepia", 	{16#9E,16#5B,16#40}).
+?COLORNAME("Fuzzy Wuzzy", 	{16#87,16#42,16#1F}).
+?COLORNAME("Beaver", 	{16#92,16#6F,16#5B}).
+?COLORNAME("Tumbleweed", 	{16#DE,16#A6,16#81}).
+?COLORNAME("Raw Sienna", 	{16#D2,16#7D,16#46}).
+?COLORNAME("Van Dyke Brown", 	{16#66,16#42,16#28}).
+?COLORNAME("Tan", 	{16#D9,16#9A,16#6C}).
+?COLORNAME("Desert Sand", 	{16#ED,16#C9,16#AF}).
+?COLORNAME("Peach", 	{16#FF,16#CB,16#A4}).
+?COLORNAME("Burnt Umber", 	{16#80,16#55,16#33}).
+?COLORNAME("Apricot", 	{16#FD,16#D5,16#B1}).
+?COLORNAME("Almond", 	{16#EE,16#D9,16#C4}).
+?COLORNAME("Raw Umber", 	{16#66,16#52,16#33}).
+?COLORNAME("Shadow", 	{16#83,16#70,16#50}).
+?COLORNAME("Raw Sienna (I)", 	{16#E6,16#BC,16#5C}).
+?COLORNAME("Timberwolf", 	{16#D9,16#D6,16#CF}).
+?COLORNAME("Gold (I)", 	{16#92,16#92,16#6E}).
+?COLORNAME("Gold (II)", {16#E6,16#BE,16#8A}).
+?COLORNAME("Silver", 	{16#C9,16#C0,16#BB}).
+?COLORNAME("Copper", 	{16#DA,16#8A,16#67}).
+?COLORNAME("Antique Brass", 	{16#C8,16#8A,16#65}).
+?COLORNAME("Black", 	{16#00,16#00,16#00}).
+?COLORNAME("Charcoal Gray", 	{16#73,16#6A,16#62}).
+?COLORNAME("Gray", 	{16#8B,16#86,16#80}).
+?COLORNAME("Blue-Gray", 	{16#C8,16#C8,16#CD}).
+?COLORNAME("Radical Red", 	{16#FF,16#35,16#5E}).
+?COLORNAME("Wild Watermelon", 	{16#FD,16#5B,16#78}).
+?COLORNAME("Outrageous Orange", 	{16#FF,16#60,16#37}).
+?COLORNAME("Atomic Tangerine", 	{16#FF,16#99,16#66}).
+?COLORNAME("Neon Carrot", 	{16#FF,16#99,16#33}).
+?COLORNAME("Sunglow", 	{16#FF,16#CC,16#33}).
+?COLORNAME("Laser Lemon", 	{16#FF,16#FF,16#66}).
+?COLORNAME("Unmellow Yellow", 	{16#FF,16#FF,16#66}).
+?COLORNAME("Electric Lime", 	{16#CC,16#FF,16#00}).
+?COLORNAME("Screamin' Green", 	{16#66,16#FF,16#66}).
+?COLORNAME("Magic Mint", 	{16#AA,16#F0,16#D1}).
+?COLORNAME("Blizzard Blue", 	{16#50,16#BF,16#E6}).
+?COLORNAME("Shocking Pink", 	{16#FF,16#6E,16#FF}).
+?COLORNAME("Razzle Dazzle Rose", 	{16#EE,16#34,16#D2}).
+?COLORNAME("Hot Magenta", 	{16#FF,16#00,16#CC}).
+?COLORNAME("Purple Pizzazz", 	{16#FF,16#00,16#CC}).
+?COLORNAME("Sizzling Red", 	{16#FF,16#38,16#55}).
+?COLORNAME("Red Salsa", 	{16#FD,16#3A,16#4A}).
+?COLORNAME("Tart Orange", 	{16#FB,16#4D,16#46}).
+?COLORNAME("Orange Soda", 	{16#FA,16#5B,16#3D}).
+?COLORNAME("Bright Yellow", 	{16#FF,16#AA,16#1D}).
+?COLORNAME("Yellow Sunshine", 	{16#FF,16#F7,16#00}).
+?COLORNAME("Slimy Green", 	{16#29,16#96,16#17}).
+?COLORNAME("Green Lizard", 	{16#A7,16#F4,16#32}).
+?COLORNAME("Denim Blue", 	{16#22,16#43,16#B6}).
+?COLORNAME("Blue Jeans", 	{16#5D,16#AD,16#EC}).
+?COLORNAME("Plump Purple", 	{16#59,16#46,16#B2}).
+?COLORNAME("Purple Plum", 	{16#9C,16#51,16#B6}).
+?COLORNAME("Sweet Brown", 	{16#A8,16#37,16#31}).
+?COLORNAME("Brown Sugar", 	{16#AF,16#6E,16#4D}).
+?COLORNAME("Eerie Black", 	{16#1B,16#1B,16#1B}).
+?COLORNAME("Black Shadows", 	{16#BF,16#AF,16#B2}).
+?COLORNAME("Fiery Rose", 	{16#FF,16#54,16#70}).
+?COLORNAME("Sizzling Sunrise", 	{16#FF,16#DB,16#00}).
+?COLORNAME("Heat Wave", 	{16#FF,16#7A,16#00}).
+?COLORNAME("Lemon Glacier", 	{16#FD,16#FF,16#00}).
+?COLORNAME("Spring Frost", 	{16#87,16#FF,16#2A}).
+?COLORNAME("Absolute Zero", 	{16#00,16#48,16#BA}).
+?COLORNAME("Winter Sky", 	{16#FF,16#00,16#7C}).
+?COLORNAME("Frostbite", 	{16#E9,16#36,16#A7}).
+?COLORNAME("Alloy Orange", 	{16#C4,16#62,16#10}).
+?COLORNAME("B'dazzled Blue", 	{16#2E,16#58,16#94}).
+?COLORNAME("Big Dip O' Ruby", 	{16#9C,16#25,16#42}).
+?COLORNAME("Bittersweet Shimmer", 	{16#BF,16#4F,16#51}).
+?COLORNAME("Blast Off Bronze", 	{16#A5,16#71,16#64}).
+?COLORNAME("Cyber Grape", 	{16#58,16#42,16#7C}).
+?COLORNAME("Deep Space Sparkle", 	{16#4A,16#64,16#6C}).
+?COLORNAME("Gold Fusion", 	{16#85,16#75,16#4E}).
+?COLORNAME("Illuminating Emerald", 	{16#31,16#91,16#77}).
+?COLORNAME("Metallic Seaweed", 	{16#0A,16#7E,16#8C}).
+?COLORNAME("Metallic Sunburst", 	{16#9C,16#7C,16#38}).
+?COLORNAME("Razzmic Berry", 	{16#8D,16#4E,16#85}).
+?COLORNAME("Sheen Green", 	{16#8F,16#D4,16#00}).
+?COLORNAME("Shimmering Blush", 	{16#D9,16#86,16#95}).
+?COLORNAME("Sonic Silver", 	{16#75,16#75,16#75}).
+?COLORNAME("Steel Blue", 	{16#00,16#81,16#AB}).
+?COLORNAME("Aztec Gold", 	{16#C3,16#99,16#53}).
+?COLORNAME("Burnished Brown", 	{16#A1,16#7A,16#74}).
+?COLORNAME("Cerulean Frost", 	{16#6D,16#9B,16#C3}).
+?COLORNAME("Cinnamon Satin", 	{16#CD,16#60,16#7E}).
+?COLORNAME("Copper Penny", 	{16#AD,16#6F,16#69}).
+?COLORNAME("Cosmic Cobalt", 	{16#2E,16#2D,16#88}).
+?COLORNAME("Glossy Grape", 	{16#AB,16#92,16#B3}).
+?COLORNAME("Granite Gray", 	{16#67,16#67,16#67}).
+?COLORNAME("Green Sheen", 	{16#6E,16#AE,16#A1}).
+?COLORNAME("Lilac Luster", 	{16#AE,16#98,16#AA}).
+?COLORNAME("Misty Moss", 	{16#BB,16#B4,16#77}).
+?COLORNAME("Mystic Maroon", 	{16#AD,16#43,16#79}).
+?COLORNAME("Pearly Purple", 	{16#B7,16#68,16#A2}).
+?COLORNAME("Pewter Blue", 	{16#8B,16#A8,16#B7}).
+?COLORNAME("Polished Pine", 	{16#5D,16#A4,16#93}).
+?COLORNAME("Quick Silver", 	{16#A6,16#A6,16#A6}).
+?COLORNAME("Rose Dust", 	{16#9E,16#5E,16#6F}).
+?COLORNAME("Rusty Red", 	{16#DA,16#2C,16#43}).
+?COLORNAME("Shadow Blue", 	{16#77,16#8B,16#A5}).
+?COLORNAME("Shiny Shamrock", 	{16#5F,16#A7,16#78}).
+?COLORNAME("Steel Teal", 	{16#5F,16#8A,16#8B}).
+?COLORNAME("Sugar Plum", 	{16#91,16#4E,16#75}).
+?COLORNAME("Twilight Lavender", 	{16#8A,16#49,16#6B}).
+?COLORNAME("Wintergreen Dream", 	{16#56,16#88,16#7D}).
+?COLORNAME("Baby Powder", 	{16#FE,16#FE,16#FA}).
+?COLORNAME("Banana", 	{16#FF,16#D1,16#2A}).
+?COLORNAME("Blueberry", 	{16#4F,16#86,16#F7}).
+?COLORNAME("Bubble Gum", 	{16#FF,16#D3,16#F8}).
+?COLORNAME("Cedar Chest", 	{16#C9,16#5A,16#49}).
+?COLORNAME("Cherry", 	{16#DA,16#26,16#47}).
+?COLORNAME("Chocolate", 	{16#BD,16#82,16#60}).
+?COLORNAME("Coconut", 	{16#FE,16#FE,16#FE}).
+?COLORNAME("Daffodil", 	{16#FF,16#FF,16#31}).
+?COLORNAME("Dirt", 	{16#9B,16#76,16#53}).
+?COLORNAME("Eucalyptus", 	{16#44,16#D7,16#A8}).
+?COLORNAME("Fresh Air", 	{16#A6,16#E7,16#FF}).
+?COLORNAME("Grape", 	{16#6F,16#2D,16#A8}).
+?COLORNAME("Jelly Bean", 	{16#DA,16#61,16#4E}).
+?COLORNAME("Leather Jacket", 	{16#25,16#35,16#29}).
+?COLORNAME("Lemon", 	{16#FF,16#FF,16#38}).
+?COLORNAME("Licorice", 	{16#1A,16#11,16#10}).
+?COLORNAME("Lilac", 	{16#DB,16#91,16#EF}).
+?COLORNAME("Lime", 	{16#B2,16#F3,16#02}).
+?COLORNAME("Lumber", 	{16#FF,16#E4,16#CD}).
+?COLORNAME("New Car", 	{16#21,16#4F,16#C6}).
+?COLORNAME("Orange", 	{16#FF,16#88,16#66}).
+?COLORNAME("Peach", 	{16#FF,16#D0,16#B9}).
+?COLORNAME("Pine", 	{16#45,16#A2,16#7D}).
+?COLORNAME("Rose", 	{16#FF,16#50,16#50}).
+?COLORNAME("Shampoo", 	{16#FF,16#CF,16#F1}).
+?COLORNAME("Smoke", 	{16#73,16#82,16#76}).
+?COLORNAME("Soap", 	{16#CE,16#C8,16#EF}).
+?COLORNAME("Strawberry", 	{16#FC,16#5A,16#8D}).
+?COLORNAME("Tulip", 	{16#FF,16#87,16#8D}).
+?COLORNAME("Amethyst",	{16#64,16#60,16#9A}).
+?COLORNAME("Citrine", 	{16#93,16#37,16#09}).
+?COLORNAME("Emerald", 	{16#14,16#A9,16#89}).
+?COLORNAME("Jade", 	{16#46,16#9A,16#84}).
+?COLORNAME("Jasper", 	{16#D0,16#53,16#40}).
+?COLORNAME("Lapis Lazuli", 	{16#43,16#6C,16#B9}).
+?COLORNAME("Malachite", 	{16#46,16#94,16#96}).
+?COLORNAME("Moonstone", 	{16#3A,16#A8,16#C1}).
+?COLORNAME("Onyx", 	{16#35,16#38,16#39}).
+?COLORNAME("Peridot", 	{16#AB,16#AD,16#48}).
+?COLORNAME("Pink Pearl", 	{16#B0,16#70,16#80}).
+?COLORNAME("Rose Quartz", 	{16#BD,16#55,16#9C}).
+?COLORNAME("Ruby", 	{16#AA,16#40,16#69}).
+?COLORNAME("Sapphire", 	{16#2D,16#5D,16#A1}).
+?COLORNAME("Smokey Topaz", 	{16#83,16#2A,16#0D}).
+?COLORNAME("Tiger's Eye", 	{16#B5,16#69,16#17}).
+?COLORNAME("Baseball Mitt (Burnt Sienna)", {16#E9,16#74,16#51}).
+?COLORNAME("Bubble Bath (Tickle Me Pink)", {16#FC,16#80,16#A5}).
+?COLORNAME("Earthworm (Brick Red)", 	{16#C6,16#2D,16#42}).
+?COLORNAME("Flower Shop (Wisteria)", 	{16#C9,16#A0,16#DC}).
+?COLORNAME("Fresh Air (Sky Blue)", 	{16#76,16#D7,16#EA}).
+?COLORNAME("Grandma's Perfume (Orange)", {16#FF,16#88,16#33}).
+?COLORNAME("Koala Tree (Jungle Green)", {16#29,16#AB,16#87}).
+?COLORNAME("Pet Shop (Brown)", 	{16#AF,16#59,16#3E}).
+?COLORNAME("Pine Tree (Pine Green)", 	{16#01,16#78,16#6F}).
+?COLORNAME("Saw Dust (Peach)", 	{16#FF,16#CB,16#A4}).
+?COLORNAME("Sharpening Pencils (Goldenrod)", 	{16#FC,16#D6,16#67}).
+?COLORNAME("Smell the Roses (Red)", 	{16#ED,16#0A,16#3F}).
+?COLORNAME("Sunny Day (Yellow)", 	{16#FB,16#E8,16#70}).
+?COLORNAME("Wash the Dog (Dandelion)", 	{16#FE,16#D8,16#5D}).
+?COLORNAME("Alien Armpit", 	{16#84,16#DE,16#02}).
+?COLORNAME("Big Foot Feet", 	{16#E8,16#8E,16#5A}).
+?COLORNAME("Booger Buster", 	{16#DD,16#E2,16#6A}).
+?COLORNAME("Dingy Dungeon", 	{16#C5,16#31,16#51}).
+?COLORNAME("Gargoyle Gas", 	{16#FF,16#DF,16#46}).
+?COLORNAME("Giant's Club", 	{16#B0,16#5C,16#52}).
+?COLORNAME("Magic Potion", 	{16#FF,16#44,16#66}).
+?COLORNAME("Mummy's Tomb", 	{16#82,16#8E,16#84}).
+?COLORNAME("Ogre Odor", 	{16#FD,16#52,16#40}).
+?COLORNAME("Pixie Powder", 	{16#39,16#12,16#85}).
+?COLORNAME("Princess Perfume", 	{16#FF,16#85,16#CF}).
+?COLORNAME("Sasquatch Socks", 	{16#FF,16#46,16#81}).
+?COLORNAME("Sea Serpent", 	{16#4B,16#C7,16#CF}).
+?COLORNAME("Smashed Pumpkin", 	{16#FF,16#6D,16#3A}).
+?COLORNAME("Sunburnt Cyclops", 	{16#FF,16#40,16#4C}).
+?COLORNAME("Winter Wizard", 	{16#A0,16#E6,16#FF}).
